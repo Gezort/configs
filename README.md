137114175a8302190c2c9f1e3006173b0dbcb53b
Gezort <miron_55@mail.ru>
2018-06-25 12:00:03 +0300

    add description

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # configs
+
+```shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/install_tools/install.sh)"
+```
---
+++ install_tools/install.sh
@@ -0,0 +1,2 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/install_tools/vim-config.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/install_tools/zsh-config.sh)"
