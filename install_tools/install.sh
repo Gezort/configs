26ee8e79de17a73210630c17ac565df1e524e506
Gezort <Gezort@users.noreply.github.com>
2018-10-04 22:35:29 +0300

    Update install.sh

--- install_tools/install.sh
+++ install_tools/install.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/install_tools/vim-config.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/install_tools/zsh-config.sh)"
+echo "exec /bin/zsh" >> ~/.bashrc
