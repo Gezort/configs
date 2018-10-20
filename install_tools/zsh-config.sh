da4c8c6ad827a6ff2d08944ebf19dd01c2d52955
Gezort <Gezort@users.noreply.github.com>
2018-10-20 11:24:06 +0300

    Update zsh-config.sh

--- install_tools/zsh-config.sh
+++ install_tools/zsh-config.sh
@@ -4,6 +4,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 echo "ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=red'" > ~/.oh-my-zsh/custom/zsh-autosuggestions-config.zsh
 
-curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/conf_sources/.vimrc > ~/.vimrc
 curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/conf_sources/.zshrc > ~/.zshrc
 curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/conf_sources/.pythonrc > ~/.pythonrc
