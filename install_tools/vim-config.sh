9028661e4be079ad42b61f377d922198741fbe79
Gezort <Gezort@users.noreply.github.com>
2018-10-20 11:38:45 +0300

    Update vim-config.sh

--- install_tools/vim-config.sh
+++ install_tools/vim-config.sh
@@ -1,4 +1,3 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
 curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/conf_sources/.vimrc > ~/.vimrc && \
-vim +PluginInstall +qall || \
-rm -rf ~/.vim ~/.vimrc
+vim +PluginInstall +qall
