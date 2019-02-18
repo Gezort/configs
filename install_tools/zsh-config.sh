9d575e051cf49acb771e63ba165046765135479e
Gezort <Gezort@users.noreply.github.com>
2019-02-18 17:22:53 +0300

    Update zsh-config.sh

--- install_tools/zsh-config.sh
+++ install_tools/zsh-config.sh
@@ -1,3 +1,5 @@
+sudo apt-get install zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
