fa5ad9521b5fdaca8c29ebc6a7b3c109bcc5566b
Gezort <Gezort@users.noreply.github.com>
2019-09-09 10:53:17 +0300

    Update install.sh

--- install_tools/install.sh
+++ install_tools/install.sh
@@ -1,3 +1,3 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/install_tools/zsh-config.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/install_tools/zsh-config.sh)" && \
 echo "exec /bin/zsh" >> ~/.bashrc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/gezort/configs/master/install_tools/vim-config.sh)"
